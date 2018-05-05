91a90079f76f7aaf84e3785fd7ad3ce51631ef52
Tristan Mitchell <tmitche9@nd.edu>
2018-05-04 22:56:02 -0400

    Added more regressors and classifiers with better results

--- classifiers.py
+++ classifiers.py
@@ -2,32 +2,40 @@ import pandas as pd
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 
 training_data_filename = "classification_train.data"
-data = pd.read_csv(training_data_filename, header = None)
+data = pd.read_csv(training_data_filename, header=None)
 
 y = data[48]
 x = data.drop([48], axis=1)
 
+classifiers = list()
+classifiers.append(MLPClassifier())
+classifiers.append(DecisionTreeClassifier())
+classifiers.append(KNeighborsClassifier())
+classifiers.append(AdaBoostClassifier())
+
 skf = StratifiedKFold(n_splits=10)
+fold = 1
 
 for train_index, test_index in skf.split(x, y):
+    print "Fold {}\n-------".format(fold)
+    fold = fold + 1
+
     x_train = data.loc[train_index, list(range(48))]
     x_test = data.loc[test_index, list(range(48))]
     y_train = data.loc[train_index, [48]]
     y_test = data.loc[test_index, [48]]
 
-    classifier1 = MLPClassifier()
-    classifier1.fit(x_train, y_train)
-    y_pred1 = classifier1.predict(x_test)
+    for classifier in classifiers:
+        classifier.fit(x_train, y_train)
+        y_pred = classifier.predict(x_test)
+        accuracy = accuracy_score(y_pred, y_test)
+        print "{} Accuracy: {}".format(classifier.__class__.__name__, accuracy)
 
-    classifier2 = DecisionTreeClassifier()
-    classifier2.fit(x_train, y_train)
-    y_pred2 = classifier2.predict(x_test)
-
-    c1_accuracy_score = accuracy_score(y_pred1, y_test)
-    c2_accuracy_score = accuracy_score(y_pred2, y_test)
-
-    print "MLP Accuracy: {}\t Decision Tree Accuracy: {}".format(c1_accuracy_score, c2_accuracy_score)
+    print ""
--- regressors.py
+++ regressors.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error
 
@@ -11,23 +13,28 @@ data = pd.read_csv(training_data_filename, header=None)
 y = data[21]
 x = data.drop([21], axis=1)
 
+regressors = list()
+regressors.append(LinearRegression())
+regressors.append(AdaBoostRegressor())
+regressors.append(RandomForestRegressor())
+regressors.append(KNeighborsRegressor())
+
 skf = StratifiedKFold(n_splits=10)
+fold = 1
 
 for train_index, test_index in skf.split(x, y):
+    print "Fold {}\n-------".format(fold)
+    fold = fold + 1
+
     x_train = data.loc[train_index, list(range(21))]
     x_test = data.loc[test_index, list(range(21))]
     y_train = data.loc[train_index, [21]]
     y_test = data.loc[test_index, [21]]
 
-    regressor1 = LinearRegression()
-    regressor1.fit(x_train, y_train)
-    y_pred1 = regressor1.predict(x_test)
+    for regressor in regressors:
+        regressor.fit(x_train, y_train)
+        y_pred = regressor.predict(x_test)
+        mse = mean_squared_error(y_pred, y_test)
+        print "{} MSE: {}".format(regressor.__class__.__name__, mse)
 
-    regressor2 = AdaBoostRegressor()
-    regressor2.fit(x_train, y_train)
-    y_pred2 = regressor2.predict(x_test)
-
-    r1_mse = mean_squared_error(y_pred1, y_test)
-    r2_mse = mean_squared_error(y_pred2, y_test)
-
-    print "Linear Regressor MSE: {}\t AdaBoost Regressor MSE: {}".format(r1_mse, r2_mse)
+    print ""
