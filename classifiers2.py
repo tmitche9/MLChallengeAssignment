a0c39dd7868a872328cbdbb9f7af74b7c9c7de0d
John McGuinness <johnmcguinness@johns-mbp.dhcp.nd.edu>
2018-05-06 18:11:53 -0400

    optimal point found for AdaBoost

--- classifiers2.py
+++ classifiers2.py
@@ -12,13 +12,14 @@ y = data[48]
 x = data.drop([48], axis=1)
 
 classifiers = list()
-classifiers.append(AdaBoostClassifier())
-classifiers.append(AdaBoostClassifier(n_estimators=100))
-classifiers.append(AdaBoostClassifier(n_estimators=10))
+classifiers.append(AdaBoostClassifier(n_estimators=100, learning_rate=1.2))
+classifiers.append(AdaBoostClassifier(n_estimators=100, learning_rate=1.2))
+classifiers.append(AdaBoostClassifier(n_estimators=100, learning_rate=1.2))
 
 skf = StratifiedKFold(n_splits=10)
 fold = 1
 
+scores = [0, 0, 0]
 for train_index, test_index in skf.split(x, y):
     print "Fold {}\n-------".format(fold)
     fold = fold + 1
@@ -28,10 +29,18 @@ for train_index, test_index in skf.split(x, y):
     y_train = data.loc[train_index, [48]]
     y_test = data.loc[test_index, [48]]
 
-    for classifier in classifiers:
+    for k, classifier in enumerate(classifiers):
         classifier.fit(x_train, y_train)
         y_pred = classifier.predict(x_test)
         accuracy = accuracy_score(y_pred, y_test)
+        scores[k] += accuracy
         print "{} Accuracy: {}".format(classifier.__class__.__name__, accuracy)
-
+    
     print ""
+
+#get mean value
+scores = [1.*score/10 for score in scores]
+
+for score in scores:
+    print "Overall Accuracy: {}".format(score)
+
